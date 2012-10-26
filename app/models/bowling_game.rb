26cb2758f7f26bdbc5fe5292a9a04026a3e9e7cd
Chris Tweney <chris@media.berkeley.edu>
2012-10-26 10:27:00 -0700

    Different bowling game impl

--- app/models/bowling_game.rb
+++ app/models/bowling_game.rb
@@ -1,30 +1,35 @@
 class BowlingGame
 
 	def initialize
-		@rolls = Array.new(21, 0)
+		@score = 0
+		@rolls = Array.new(22, 0)
 		@current_roll = 0
 	end
 
+	def roll(pins)
+		@rolls[@current_roll] = pins
+		@current_roll += 1
+	end
+
 	def score
-		@score = 0
+		score = 0
 		frame_index = 0
-		p @rolls
-		for frame in 0...10 do
+		for frame in 0..9
 			if is_strike(frame_index)
-				@score += 10 + strike_bonus(frame_index)
+				score += 10 + strike_bonus(frame_index)
 				frame_index += 1
 			elsif is_spare(frame_index)
-				@score += 10 + spare_bonus(frame_index)
+				score += 10 + spare_bonus(frame_index)
 				frame_index += 2
 			else
-				@score += sum_of_balls_in_frame(frame_index)
+				score += sum_of_frame(frame_index)
 				frame_index += 2
 			end
 		end
-		@score
+		score
 	end
 
-	def sum_of_balls_in_frame(frame_index)
+	def sum_of_frame(frame_index)
 		@rolls[frame_index] + @rolls[frame_index+1]
 	end
 
@@ -43,10 +48,4 @@ class BowlingGame
 	def is_spare(frame_index)
 		@rolls[frame_index] + @rolls[frame_index+1] == 10
 	end
-
-	def roll(pins)
-		@rolls[@current_roll] = pins
-		@current_roll += 1
-	end
-
 end
--- test/unit/bowling_test.rb
+++ test/unit/bowling_test.rb
@@ -1,43 +1,46 @@
 require 'test_helper'
 
-class BowlingGameTest < ActiveSupport::TestCase
+class BowlingTest < ActiveSupport::TestCase
 
 	def setup
-		@game = BowlingGame.new
+	   @g = BowlingGame.new
 	end
 
-	def roll_many(_pins, _times)
-		for i in 1.._times
-			@game.roll(_pins)
+	def roll_many(pins, times)
+		for i in 1..times
+			@g.roll(pins)
 		end
 	end
 
 	test "all gutterballs" do
 		roll_many(0, 20)
-		assert_equal(0, @game.score)
+		assert_equal(0, @g.score)
 	end
 
 	test "all ones" do
-		@game = BowlingGame.new
-		assert_equal(0, @game.score)
 		roll_many(1, 20)
-		assert_equal(20, @game.score)
+		assert_equal(20, @g.score)
 	end
 
 	test "one spare" do
-		@game.roll(5)
-		@game.roll(5) # spare
-		@game.roll(3)
+		@g.roll(5)
+		@g.roll(5) # spare
+		@g.roll(3)
 		roll_many(0, 17)
-		assert_equal(16, @game.score)
+		assert_equal(16, @g.score)
 	end
 
 	test "one strike" do
-		@game.roll(10) # strike
-		@game.roll(3)
-		@game.roll(4)
+		@g.roll(10) # strike
+		@g.roll(3)
+		@g.roll(4)
 		roll_many(0, 16)
-		assert_equal(24, @game.score)
+		assert_equal(24, @g.score)
+	end
+
+	test "perfect game" do
+		roll_many(10, 12)
+		assert_equal(300, @g.score)
 	end
 
 end
